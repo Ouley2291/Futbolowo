e79ad6b70b9983f071431799904c0276ec9cba12
Oliwier <olipl2291@gmail.com>
2026-05-14 15:24:51 +0200

    Dodano funkcje odpowiadającą za sortowanie graczy wedlug pozycji w players.models.py

--- apps/players/models.py
+++ apps/players/models.py
@@ -49,7 +49,41 @@ class Player(models.Model):
         today = date.today()
         return (today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day)))
 
+def sort_bys_position(players):
+    new_list = []
+    gk = players.filter(position="GK")
+    cb = players.filter(position="CB")
+    lb = players.filter(position="LB")
+    rb = players.filter(position="RB")
+    lwb = players.filter(position="LWB")
+    rwb = players.filter(position="RWB")
+    cdm = players.filter(position="CDM")
+    cm = players.filter(position="CM")
+    cam = players.filter(position="CAM")
+    lm = players.filter(position="LM")
+    rm = players.filter(position="RM")
+    lw = players.filter(position="LW")
+    rw = players.filter(position="RW")
+    cf = players.filter(position="CF")
+    st = players.filter(position="ST")
 
+    new_list.extend(gk)
+    new_list.extend(cb)
+    new_list.extend(lb)
+    new_list.extend(rb)
+    new_list.extend(lwb)
+    new_list.extend(rwb)
+    new_list.extend(cdm)
+    new_list.extend(cm)
+    new_list.extend(cam)
+    new_list.extend(lm)
+    new_list.extend(rm)
+    new_list.extend(lw)
+    new_list.extend(rw)
+    new_list.extend(cf)
+    new_list.extend(st)
+
+    return new_list
 
 
 
--- apps/players/views.py
+++ apps/players/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Player
+from .models import Player, sort_bys_position
 
 def team_stats(request):
-    players = Player.objects.all()
+    players = Player.objects.all().order_by("number")
+    players = sort_bys_position(players)
 
     return render(request, 'players/team_stats.html', {
         "players": players,
