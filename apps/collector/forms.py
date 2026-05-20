24196c79e7c5a389622db18c9c9a9435949f9f6d
Oliwier <olipl2291@gmail.com>
2026-05-20 13:55:36 +0200

    Jednak model LeagueQuery nie jest potrzebnye wiec zmienilem LeagueQueryForm na zwykly formularz

--- apps/collector/forms.py
+++ apps/collector/forms.py
@@ -1,7 +1,31 @@
-from django.forms import ModelForm
+from django import forms
 from .models import LeagueQuery
 
-class LeagueQueryForm(ModelForm):
-    class Meta:
-        model = LeagueQuery
-        fields = ['season', 'province', 'competition_class', 'group']
+class LeagueQueryForm(forms.Form):    
+    SEASON_CHOICES = [
+        ("2023/2024", "2023/2024"),
+        ("2024/2025", "2024/2025"),
+        ("2025/2026", "2025/2026"),
+    ]
+
+    PROVINCE_CHOICES = [
+        ("dolnośląskie", "Dolnośląskie"),
+        ("kujawsko-pomorskie", "Kujawsko-Pomorskie"),
+        ("lubelskie", "Lubelskie"),
+        ("wielkopolskie", "Wielkopolskie"),
+    ]
+
+    COMPETITION_CLASS_CHOICES = [
+        ("Czwarta liga", "Czwarta liga"),
+        ("Piąta liga", "Piąta liga"),
+        ("Klasa okręgowa", "Klasa okręgowa"),
+        ("Klasa A", "Klasa A"),
+        ("Klasa B", "Klasa B"),
+        ("Klasa C", "Klasa C"),
+    ]
+
+    season = forms.ChoiceField(choices=SEASON_CHOICES)
+    province = forms.ChoiceField(choices=PROVINCE_CHOICES)
+    competition_class = forms.ChoiceField(choices=COMPETITION_CLASS_CHOICES)
+    group = forms.CharField(max_length=100)
+
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 
-from apps.collector.models import LeagueQuery
 from apps.collector.forms import LeagueQueryForm
 
 def index(request):
     if request.method == "POST":
         form = LeagueQueryForm(request.POST)
         if form.is_valid():
-            form.save()
+            data = form.cleaned_data
+            data["league_type"] = "Niższe ligi"
+
+            print(data)
+
             return redirect("core:index")
     else:
         form = LeagueQueryForm()
