98d88f31f35e7645069750eb12a5d6c45444855c
Hubert Koszyk <hubertkoszyk@zohomail.eu>
2026-06-11 09:18:47 +0200

    COS DZIALKA

--- apps/web_scrapping/main_scrapper.py
+++ apps/web_scrapping/main_scrapper.py
@@ -56,23 +56,20 @@ def scrape_laczynaspilka(liga, runda=None, kolejka=None, wojewodztwo=None, klasa
 
         # twożenie typowego clienta ala google chrome
         browser = p.chromium.launch(
-            channel="chrome",
             headless=True,
             slow_mo=300,
             args=["--disable-blink-features=AutomationControlled"]
         )
         context = browser.new_context(
             viewport={'width': 1920, 'height': 1080},
-            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
         )
-        context.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        # context.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
         # twożenie i ładowanie obiektu strony
         page = context.new_page()
-        print("Ładuję główną stronę w tle (inicjalizacja sesji)...")
-        page.goto("https://www.laczynaspilka.pl/", wait_until="networkidle")
-
-
+        print("Wymuszam przejście do zakładki Rozgrywki...")
+        page.goto("https://www.laczynaspilka.pl/rozgrywki", wait_until="networkidle")
 
         try:
             print("Sprawdzam i klikam ciasteczka...")
@@ -83,16 +80,6 @@ def scrape_laczynaspilka(liga, runda=None, kolejka=None, wojewodztwo=None, klasa
             page.wait_for_timeout(1000)
         except Exception:
             print(" -> Brak ciasteczek, jedziemy dalej.")
-
-        print("Wymuszam przejście do zakładki Rozgrywki...")
-        page.evaluate('''() => {
-            let linki = document.querySelectorAll("a[href='/rozgrywki'], a[href='https://www.laczynaspilka.pl/rozgrywki']");
-            if (linki.length > 0) {
-                linki[0].click();
-            } else {
-                window.location.href = '/rozgrywki';
-            }
-        }''')
 
         try:
             page.get_by_text("Sezon", exact=True).locator("visible=true").first.wait_for(state="visible", timeout=15000)
@@ -133,12 +120,14 @@ def scrape_laczynaspilka(liga, runda=None, kolejka=None, wojewodztwo=None, klasa
 
             wybierz_parametr("Sezon", sezon)
             wybierz_parametr("Liga", liga)
-            wybierz_parametr("Runda", runda)
-            wybierz_parametr("Kolejka", kolejka)
+            if runda:
+                wybierz_parametr("Runda", runda)
+            if kolejka:
+                wybierz_parametr("Kolejka", kolejka)
 
             if liga == "Niższe ligi":
                 wybierz_parametr("Województwa", wojewodztwo)
-                wybierz_parametr("Klasa rozgrywkowa", klasa)
+                wybierz_parametr("Rozgrywka", klasa)
                 wybierz_parametr("Grupa", grupa)
 
             # ---------------------------
@@ -188,13 +177,13 @@ def scrape_laczynaspilka(liga, runda=None, kolejka=None, wojewodztwo=None, klasa
 
 def main():
     #TESTY UŻYCIA
-    wybrana_liga = "Niższe ligi"  # Tutaj wpisujecie wybraną lige
+    wybrana_liga = "Ekstraklasa"  # Tutaj wpisujecie wybraną lige
 
     if wybrana_liga == "Ekstraklasa":
         dane_json = scrape_laczynaspilka(
             liga="Ekstraklasa",
             runda="Wiosenna",
-            kolejka="33 (bieżąca)"
+            kolejka="34 (bieżąca)"
         )
     else:
         dane_json = scrape_laczynaspilka(
